c3a77c4d223382e3cee95b401429440a4a0d828a
Letícia Fabri <leticiafabri10@gmail.com>
2025-09-06 17:23:20 -0300

    Update ExercicioAnoNascimento.asm

--- ExercicioAnoNascimento.asm
+++ ExercicioAnoNascimento.asm
@@ -6,27 +6,27 @@
 main:
 li $v0, 4 #4= system code for printing a string, 
 lw $t1, num2
-la $a0,prompt # load address of prompt in $a0
-syscall # prints the string � Enter your age�
+la $a0,prompt
+syscall 
 
 
 # pega o input do usuario
 li $v0,5 #5= system code for user input
 syscall #Call to the System to execute the instruction
-move $t0, $v0 # Store the result in $t0
+move $t0, $v0 
 
 add $t2, $t0, $t1
 
 # Display the user input
-li $v0, 4 #4= system code for printing a string, 
+li $v0, 4 
 
-la $a0, message # load address of prompt in $a0
-syscall # prints the string � Your age is�
+la $a0, message 
+syscall
 # Show the age
 li $v0, 1 #1= system code for printing a word, 
-move $a0, $t2 # move is a pseudo-instruction that transfers contents of $t0 to $a0
-#a0 is the register that needs to hold the value that needs to be printed
-syscall #Call to the System to execute our instructions 
+move $a0, $t2 # transfere valor do t2 para o a0
+#a0 tem o valor que vai ser printado
+syscall #Chama o sistema para executar
 
 li $v0, 10
 syscall
